8c8c7f6bb19c00a4222f3e3a7a08bf1619391c06
pjimen5 <priscilla.jimenez.p@gmail.com>
2019-12-19 20:58:25 -0500

    ObjetoPersona

---
+++ CapaDatos/PersonaDAO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class PersonaDAO
+    {
+         //Metodos CRUD
+         
+        string nombre _ID;
+        string lugarNac _lugarNac;
+    }
+     
+}
