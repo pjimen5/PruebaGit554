9d3bed4d0619ef64dc8b4884ed86c721f82593ee
pjimen5 <priscilla.jimenez.p@gmail.com>
2019-12-19 22:03:18 -0500

    Metodos

--- CapaDatos/ManejadorDB.cs
+++ CapaDatos/ManejadorDB.cs
@@ -10,9 +10,17 @@ namespace CapaDatos
         // Metodo para abrir la conexión
         protected sqlConnection conexion = new SqlConnection("Server=NOMBRE_DEL_SERVIDOR;Database=NOMBRE_DE_BD;Integrated Security=true");
             
-        public void abrir_conexion() { }
+        public SqlConnection AbrirConexion() {
+             if (Conexion.State == ConnectionState.Closed)
+                 Conexion.Open();
+            return Conexion
+        }
         // Metodo para cerrar la conexión
-        public void cerrar_conexion() { }
+        public void CerrarConexion() { 
+          if (Conexion.State == ConnectionState.Open)
+                 Conexion.Close();
+            return Conexion
+        }
 
         // Metodo para ejecutar procedures
         // Metodo para hacer consultas
