764b6ed95037b482d3e721dfad0285046b189e93
pjimen5 <priscilla.jimenez.p@gmail.com>
2019-12-19 20:54:54 -0500

    StringDeConexión

--- CapaDatos/ManejadorDB.cs
+++ CapaDatos/ManejadorDB.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
@@ -6,18 +8,14 @@ namespace CapaDatos
     {
         // Se escribe la variable de conexión.
         // Metodo para abrir la conexión
-
+        protected sqlConnection conexion = new SqlConnection("Server=NOMBRE_DEL_SERVIDOR;Database=NOMBRE_DE_BD;Integrated Security=true");
+            
         public void abrir_conexion() { }
         // Metodo para cerrar la conexión
         public void cerrar_conexion() { }
 
         // Metodo para ejecutar procedures
         // Metodo para hacer consultas
-
-
-
-
-
     }
 
 }
