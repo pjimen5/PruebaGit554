ee38faca01fd9673b3a381317c29b8ee966aa20b
pjimen5 <priscilla.jimenez.p@gmail.com>
2019-12-19 21:19:47 -0500

    PersonaDAO

--- CapaDatos/PersonaDAO.cs
+++ CapaDatos/PersonaDAO.cs
@@ -3,13 +3,33 @@ using System.Data;
 using System.Data.SqlClient;
 
 namespace CapaDatos
-{
-    public class PersonaDAO
+{   
+    class PersonaDAO: ManejadorDB
     {
+       SqlDataReader leerRegistros;
+       SqlCommand comando = new SqlCommand();
+       
          //Metodos CRUD
-         
-        string nombre _ID;
-        string lugarNac _lugarNac;
+       public List<PersonaDto> VerRegistros(string _condicion)
+       {
+           comando.Connection = Conexion;
+           comando.CommandText = "VerRegistros";
+           comando.CommandType = CommandType.StoredProcedure;
+           comando.Parameters.AddWithValue("@Condicion",_condicion);
+           Conexion.Open();
+           leerRegistros = Comando.ExecuteReader();
+           List<PersonaDto> listaPersonas = new List<PersonaDto>();
+           while (leerRegistros.Read())
+           {
+              listaPersonas.Add(new PersonaDto(nombre=leerRegistros.GetString(0),lugarNac=leerRegistros.GetString(1)));
+           }
+           leerRegistros.Close();
+           Conexion.Close();
+           return listaPersonas;
+       }
+       public void Insert(){}
+       public void Edit(){}
+       public void Delete(){}
     }
      
 }
